f4c0fa69bf2005baf533ab6c98674458a06bdc27
samuelkchris <samuelkchris@gmail.com>
2024-10-30 02:14:19 +0300

    updates

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+// custom.js
+
+$(document).ready(function () {
+    // Example: Initialize select2 for better dropdowns
+    $('.select2').select2();
+
+    // Example: Initialize flatpickr for date pickers
+    $('.flatpickr').flatpickr();
+});
